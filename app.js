5085c46e62da118d4b02b44968f8f4c2848c3568
Cirqueira42 <josegilmario42@gmail.com>
2026-02-24 17:00:04 -0300

    app.js

--- app.js
+++ app.js
@@ -9,8 +9,25 @@ duracao: duracao,
 criadoEm: new Date()
 })
 .then(() => {
+
+```
+// Mensagem para WhatsApp
+const mensagem =
+  "Olá, meu nome é " + nome +
+  ". Acabei de agendar:\n" +
+  "Serviço: " + servico + "\n" +
+  "Data: " + data + "\n" +
+  "Hora: " + hora;
+
+const numero = "5516997369740";
+const url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem);
+
 alert("Agendamento realizado!");
-window.location.href = "index.html";
+
+// Abre WhatsApp
+window.location.href = url;
+```
+
 })
 .catch(error => {
 alert("Erro: " + error);
