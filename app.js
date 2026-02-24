b5e5f7ef7f3dab0c7669242ce4aba89e786cf358
Cirqueira42 <josegilmario42@gmail.com>
2026-02-24 16:02:54 -0300

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 function salvarAgendamento(nome, data, hora) {
 
-  db.collection("agendamentos").add({
-    nome: nome,
-    data: data,
-    hora: hora,
-    criadoEm: new Date()
-  })
-  .then(() => {
-    alert("Agendamento realizado com sucesso!");
-    window.location.href = "index.html";
-  })
-  .catch(error => {
-    alert("Erro ao agendar: " + error);
+  const ref = db.collection("agendamentos")
+                .where("data", "==", data)
+                .where("hora", "==", hora);
+
+  ref.get().then(snapshot => {
+
+    if (!snapshot.empty) {
+      alert("Esse horário já está ocupado!");
+      return;
+    }
+
+    db.collection("agendamentos").add({
+      nome: nome,
+      data: data,
+      hora: hora,
+      criadoEm: new Date()
+    })
+    .then(() => {
+      alert("Agendamento realizado com sucesso!");
+      window.location.href = "index.html";
+    })
+    .catch(error => {
+      alert("Erro ao agendar: " + error);
+    });
+
   });
 
 }
