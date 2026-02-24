93a0bc61c936270b484202f87d16e755bfa0ca2e
Cirqueira42 <josegilmario42@gmail.com>
2026-02-24 15:38:41 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Importações Firebase (versão compatível via CDN)
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
