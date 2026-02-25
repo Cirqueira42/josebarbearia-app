2cf3f7453421814d2bbf923c596ecc02e4c38334
Cirqueira42 <josegilmario42@gmail.com>
2026-02-25 13:50:05 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyXXXX",
   authDomain: "jose-barbearia.firebaseapp.com",
-  databaseURL: "https://jose-barbearia-default-rtdb.firebaseio.com",
   projectId: "jose-barbearia",
   storageBucket: "jose-barbearia.appspot.com",
   messagingSenderId: "123456789",
   appId: "1:123456789:web:abcd1234"
 };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
