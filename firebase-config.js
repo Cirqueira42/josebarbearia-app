dea18bbadd300374f66fc51cd1224c23d2ea32aa
Cirqueira42 <josegilmario42@gmail.com>
2026-02-25 08:56:25 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,9 @@
-// Importações Firebase (versão compatível via CDN)
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyXXXX",
+  authDomain: "jose-barbearia.firebaseapp.com",
+  databaseURL: "https://jose-barbearia-default-rtdb.firebaseio.com",
+  projectId: "jose-barbearia",
+  storageBucket: "jose-barbearia.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcd1234"
 };
-
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
